8db579add1b4383ab44814e2527c5e3c3c1e5680
kabutohacker13-byte <kabutohacker13@gmail.com>
2026-09-17 15:26:49 -0300

    revisao

---
+++ 06_Exercico.py
@@ -0,0 +1,14 @@
+# Programa para saber: Dobro, Triplo, Raiz Quadrada
+# Programador: Antonio Claudio
+# contato: kabutohacker13@gmail.com
+print('#' * (30))
+print('Dobro, Triplo, Raiz Quadrada' .center(30))
+print('#' * (30))
+dobro = 0
+triplo = 0
+riaz = 0
+numero = int(input('Digite um numero: '))
+dobro = numero * 2
+triplo = numero * 3
+raiz = numero ** (1/2)
+print(f'O dobro de é {dobro} o triplo é {triplo} e a riaz é {raiz}')
