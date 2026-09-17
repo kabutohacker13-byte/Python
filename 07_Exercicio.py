77e4419e897b29cdfc98c792afe4e058649e560d
kabutohacker13-byte <kabutohacker13@gmail.com>
2026-09-17 18:19:15 -0300

    Exercicio 07

---
+++ 07_Exercicio.py
@@ -0,0 +1,8 @@
+# Programa Média Aritmética
+# Programador : Antonio Claudio
+# Contato: kabutohaker13@gmail.com
+
+nota1 = float(input('Digite a primeira nota: '))
+nota2 = float(input('Digite a segunda nota: '))
+media = (nota1 + nota2) / 2
+print(f'O aluno teve a media {media}')
