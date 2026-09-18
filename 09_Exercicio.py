2d549359e41d3fcb735ad2118a8068aa771273b6
Claudio <kabutohacker13@gmail.com>
2026-09-17 21:01:02 -0300

    Exercicio 08

---
+++ 09_Exercicio.py
@@ -0,0 +1,13 @@
+# Programa Tabuada
+# Programador: Antonio Claudio
+# Contato: kabutohacker13@gmail.com
+# Data: 17/09/2026
+print('-' * 30)
+print('Tabuada'.center(30))
+print('-' * 30)
+
+numero = int(input("Digite um número para ver sua tabuada: "))
+
+for i in range(1, 11):
+    print(f"{numero} x {i} = {numero * i}")
+print('Programa finalizado. Volte sempre!')    
