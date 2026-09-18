f3293bf110ae3bec7d4ed18bdbeb3dfff1fa74fc
kabutohacker13-byte <kabutohacker13@gmail.com>
2026-09-18 09:09:01 -0300

    Template inicial

---
+++ template_profissional.py
@@ -0,0 +1,12 @@
+# ================================================
+# PROGRAMA: [NOME DO PROGRAMA]
+# AUTOR: Claudio
+# EMAIL: claudio@donana.com.br
+# DATA: DD/MM/YYYY
+# VERSÃO: 1.0
+# DESCRIÇÃO: [Descrição breve do que o programa faz]
+# ================================================
+
+print('=' * 30)
+print('NOME DO PROGRAMA'.center(30))
+print('=' * 30)
