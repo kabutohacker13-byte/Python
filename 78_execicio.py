2421952d596330e378738cc27cf14a0366886086
kabutohacker13-byte <kabutohacker13@gmail.com>
2026-09-23 14:49:44 -0300

    Exercicio_78_revisão02

--- 78_execicio.py
+++ 78_execicio.py
@@ -10,16 +10,18 @@
 print('=' * 30)
 print('maior  menro'.upper().center(30))
 print('=' * 30)
-numero = []
-while True:
-    num = int(input('Digite 0 para parar digite um numero: '))
-    if num == 0:
+numero = [] #cria uma lista vazia para receber o dados depois de inserido
+while True: #cria um laço que enquanto nao digitar 0 o usuario continua 
+    num = int(input('Digite 0 para parar digite um numero: ')) #entrada de dados pelo usuario 
+    if num == 0: # condição ao numero 0 para o loop
         break
-    numero.append(num)
-print(numero)
+    numero.append(num) #inseri os dados digitado pelo usuario na lista que estava vazia
+print(numero) #Exibe os numeros digitados
 
-if len(numero) > 0:
-    maior = max(numero)
-    menor = min(numero)
-print(f'O numero maior é: {maior}')
-print(f'Numero menor é : {menor}')
+if len(numero) > 0: #define se o numero maior que zero 
+    maior = max(numero) #verifica qual o numero maior e adiciona na variavel maior
+    menor = min(numero) #verifica qual o numero menor e adiciona na variavel mmenor
+    print(f'O numero maior é: {maior}') #Exibe o numero maior
+    print(f'Numero menor é : {menor}') #Exibe o numero menor
+else:
+    print('Nenhum numero foi digitado!')
